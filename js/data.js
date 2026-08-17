60f58806923feeab2f159d58205b6fbc318cf67f
2Sloo <150209172+2Sloo@users.noreply.github.com>
2026-08-16 20:14:59 -0600

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -278,7 +278,21 @@ const RELEASES = [
       mediafire: "#",
       googledrive: "#",
     },
+     // Example: images: ["assets/releaseimages/shot1.jpg", "assets/releaseimages/shot2.jpg"],
     images: [],
+     //2026-09-01    T    18:00:00         Z
+    //   date     divider    time    "this is UTC"
+// Hours, Minutes, Seconds
+   //  00:00:00 = midnight
+// 06:00:00 = 6:00 AM
+// 12:00:00 = noon
+// 18:00:00 = 6:00 PM
+// 23:00:00 = 11:00 PM
+     // THIS USES UTC TIME
+
+     // If it's during Daylight Saving Time (MDT, UTC-6) — roughly March–November — add 6 hours to convert to UTC.
+// If it's during Standard Time (MST, UTC-7) — roughly November–March — add 7 hours to convert to UTC.
+     
     releaseAt: "",
     videos: {
       tutorial: "",
@@ -312,7 +326,21 @@ const RELEASES = [
       mediafire: "#",
       googledrive: "#",
     },
+     // Example: images: ["assets/releaseimages/shot1.jpg", "assets/releaseimages/shot2.jpg"],
     images: [],
+     //2026-09-01    T    18:00:00         Z
+    //   date     divider    time    "this is UTC"
+// Hours, Minutes, Seconds
+   //  00:00:00 = midnight
+// 06:00:00 = 6:00 AM
+// 12:00:00 = noon
+// 18:00:00 = 6:00 PM
+// 23:00:00 = 11:00 PM
+     // THIS USES UTC TIME
+
+     // If it's during Daylight Saving Time (MDT, UTC-6) — roughly March–November — add 6 hours to convert to UTC.
+// If it's during Standard Time (MST, UTC-7) — roughly November–March — add 7 hours to convert to UTC.
+     
     releaseAt: "",
     videos: {
       tutorial: "",
@@ -346,7 +374,21 @@ const RELEASES = [
       mediafire: "#",
       googledrive: "#",
     },
+     // Example: images: ["assets/releaseimages/shot1.jpg", "assets/releaseimages/shot2.jpg"],
     images: [],
+     //2026-09-01    T    18:00:00         Z
+    //   date     divider    time    "this is UTC"
+// Hours, Minutes, Seconds
+   //  00:00:00 = midnight
+// 06:00:00 = 6:00 AM
+// 12:00:00 = noon
+// 18:00:00 = 6:00 PM
+// 23:00:00 = 11:00 PM
+     // THIS USES UTC TIME
+
+     // If it's during Daylight Saving Time (MDT, UTC-6) — roughly March–November — add 6 hours to convert to UTC.
+// If it's during Standard Time (MST, UTC-7) — roughly November–March — add 7 hours to convert to UTC.
+     
     releaseAt: "",
     videos: {
       tutorial: "",
@@ -514,7 +556,7 @@ const RELEASES = [
     },
    // Example: images: ["assets/releaseimages/shot1.jpg", "assets/releaseimages/shot2.jpg"],
     images: ["assets/releaseimages/2Sloo Enhanced Chase Camera Settings.png"],
-      releaseAt: "2026-08-17T02:018:00Z",
+      releaseAt: "2026-08-17T02:07:00Z",
     videos: {
       tutorial: "",
       showcase: "",
